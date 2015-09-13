633ed1bcef8f9dea33a01936f96b72e78c53ccca
mramkisoen <myron_raj@hotmail.com>
2015-09-13 21:53:15 +0200

    Update table3dp3.sql

--- Benchmark/BM3/table3dp3.sql
+++ Benchmark/BM3/table3dp3.sql
@@ -5,7 +5,7 @@ CREATE TABLE geo3d.pipe3dp3
   pipeid integer,
   attributes jsonb,
   bbox3d box3d,
-  CONSTRAINT "FK_pipe3dp_pipes" FOREIGN KEY (pipeid)
+  CONSTRAINT "FK_pipe3dp3_pipes" FOREIGN KEY (pipeid)
       REFERENCES geo3d.pipes (pipeid) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
