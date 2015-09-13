9d6b6e1fe85c907354539c021b4831fae339bd28
mramkisoen <myron_raj@hotmail.com>
2015-09-13 20:40:36 +0200

    Update ST_bboxline.sql

--- plSQLfunctions/ST_bboxline.sql
+++ plSQLfunctions/ST_bboxline.sql
@@ -24,30 +24,44 @@ DECLARE
 
  	poly geometry;
  BEGIN
- 	bpx := (((j::json->'PointBegin')->'X')::text)::float;
- 	bpy := (((j::json->'PointBegin')->'Y')::text)::float;
- 	bpz := (((j::json->'PointBegin')->'Z')::text)::float;
+ 	bpx := (((j::json->'PointBegin')->'"X"')::text)::float;
+ 	bpy := (((j::json->'PointBegin')->'"Y"')::text)::float;
+ 	bpz := (((j::json->'PointBegin')->'"Z"')::text)::float;
 
- 	epx := (((j::json->'PointEnd')->'X')::text)::float;
- 	epy := (((j::json->'PointEnd')->'Y')::text)::float;
- 	epz := (((j::json->'PointEnd')->'Z')::text)::float;
+ 	epx := (((j::json->'PointEnd')->'"X"')::text)::float;
+ 	epy := (((j::json->'PointEnd')->'"Y"')::text)::float;
+ 	epz := (((j::json->'PointEnd')->'"Z"')::text)::float;
 
  	r := ((j::json->'Radius')::text)::float;
 
- 	poly := ST_MakeLine(ST_MakePoint(bpx,bpy,bpz), ST_MakePoint(epx,epy,epz));
-  	xmin := ST_Xmin(poly);
- 	ymin := ST_Ymin(poly);
-  	zmin := ST_Zmin(poly);
-  	xmax := ST_Xmax(poly);
- 	ymax := ST_Ymax(poly);
-  	zmax := ST_Zmax(poly);
+ 	p1 := ST_MakePoint(bpx,bpy,bpz);
+ 	p2 := ST_MakePoint(epx,epy,epz);
 
-  	poly := ST_MakeBox2D(ST_MakePoint(xmin-r,ymin-r), ST_MakePoint(xmax+r,ymax+r))::geometry;
+ 	IF p1 < p2 THEN
+		poly := ST_CircelPoly02bb(j::json->'PointBegin',r,ST_3DLength(ST_MakeLine(p1,p2)),p2);
+		xmin := ST_Xmin(poly);
+		ymin := ST_Ymin(poly);
+		zmin := ST_Zmin(poly);
+		poly := ST_CircelPoly02bb(j::json->'PointEnd',r,ST_3DLength(ST_MakeLine(p1,p2)),p1);
+		xmax := ST_Xmax(poly);
+		ymax := ST_Ymax(poly);
+		zmax := ST_Zmax(poly);
+	ELSE
+		poly := ST_CircelPoly02bb(j::json->'PointEnd',r,ST_3DLength(ST_MakeLine(p1,p2)),p1);
+		xmin := ST_Xmin(poly);
+		ymin := ST_Ymin(poly);
+		zmin := ST_Zmin(poly);
+		poly := ST_CircelPoly02bb(j::json->'PointBegin',r,ST_3DLength(ST_MakeLine(p1,p2)),p2);
+		xmax := ST_Xmax(poly);
+		ymax := ST_Ymax(poly);
+		zmax := ST_Zmax(poly);
+	END IF;
 
-   	poly := ST_Force3D(poly);
-   	poly := ST_Translate(poly,0,0,zmin-r);
-    	poly := ST_Extrude(poly,0,0,((zmax+r)-(zmin-r)));	
- 	return poly;
+ 	poly := ST_MakeBox2D(ST_MakePoint(xmin,ymin),ST_MakePoint(xmax,ymax))::geometry;
+  	poly := ST_Force3D(poly);
+  	poly := ST_Translate(poly,0,0,zmin);
+   	poly := ST_Extrude(poly,0,0,(zmax-zmin));
+  	return poly;		
 END $BODY$
   LANGUAGE plpgsql IMMUTABLE
   COST 100;
