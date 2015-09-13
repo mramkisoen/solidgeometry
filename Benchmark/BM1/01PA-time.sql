75945e832fc5f3eff606e162b525c936a51c33d1
mramkisoen <myron_raj@hotmail.com>
2015-09-13 20:51:20 +0200

    Update 01PA-time.sql

--- Benchmark/BM1/01PA-time.sql
+++ Benchmark/BM1/01PA-time.sql
@@ -1,4 +1,4 @@
 EXPLAIN ANALYZE
 SELECT a.*
-FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline2(attributes), rpoint, 5)
+FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline(attributes), rpoint, 5)
 WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
