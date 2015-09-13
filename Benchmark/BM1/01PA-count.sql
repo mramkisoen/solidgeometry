4c4cc96212d8ccef5b64e8a6e89059ad0e894b5e
mramkisoen <myron_raj@hotmail.com>
2015-09-13 20:51:05 +0200

    Update 01PA-count.sql

--- Benchmark/BM1/01PA-count.sql
+++ Benchmark/BM1/01PA-count.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS interpara;
 CREATE TABLE interpara AS
 SELECT a.*
-FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline2(attributes), rpoint, 5)
+FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline(attributes), rpoint, 5)
 WHERE ST_3DDWithin(ST_3Dreconstruct(a.attributes), rpoint, 5) is true;
 
 SELECT COUNT(a.*) total, COUNT(distinct a.pipeid) totalpipeid
-FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline2(attributes), rpoint, 5)
+FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline(attributes), rpoint, 5)
 WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
