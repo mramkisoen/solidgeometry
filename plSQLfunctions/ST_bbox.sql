89fb0ddc68a1d0cc4436ab736e78cca2f4098222
mramkisoen <myron_raj@hotmail.com>
2015-09-13 21:29:42 +0200

    Update ST_bbox.sql

--- plSQLfunctions/ST_bbox.sql
+++ plSQLfunctions/ST_bbox.sql
@@ -1,3 +1,30 @@
+CREATE OR REPLACE FUNCTION st_bbox(geo geometry)
+  RETURNS geometry AS
+$BODY$
+DECLARE
+ 	poly geometry;
+ 	poly2 geometry;
+ 	zmin float;
+ 	zmax float;
+ 	h float;
+ BEGIN
+	--box3d creation
+	poly := box3d(geo)::geometry;
+--	poly2 := box3d(geo);
+	
+ 	--z min and max
+ 	zmin := ST_Zmin(geo);
+ 	zmax := ST_Zmax(geo);
+ 	h := zmax-zmin;
+ 	
+  	poly := ST_Force3D(poly);
+  	poly := ST_Translate(poly,0,0,zmin);
+  	poly := ST_Extrude(poly,0,0,h);
+ 	return poly;
+END $BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+
 CREATE OR REPLACE FUNCTION st_bbox(j jsonb)
   RETURNS geometry AS
 $BODY$
