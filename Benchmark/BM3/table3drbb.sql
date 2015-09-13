54d071bdbdb2c5384d4a4f040476a10988c8c209
mramkisoen <myron_raj@hotmail.com>
2015-09-13 21:50:12 +0200

    Create table3drbb.sql

---
+++ Benchmark/BM3/table3drbb.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS geo3d.pipe3drbb;
+
+CREATE TABLE geo3d.pipe3drbb
+(
+   pipeid integer,
+   attributes jsonb,
+   rbb geometry,
+     CONSTRAINT "FK_pipe3dp_pipes" FOREIGN KEY (pipeid)
+	REFERENCES geo3d.pipes (pipeid) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+
+INSERT INTO geo3d.pipe3drbb
+select *, ST_3DRBB(attributes)
+from geo3d.pipe3dp;
+
+CREATE INDEX pipe3drbb_index
+  ON geo3d.pipe3drbb
+  USING gist
+  (rbb);
