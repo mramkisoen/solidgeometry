79d5cf32ff4471f1edd9af7261b3bf7ac5dc3c90
mramkisoen <myron_raj@hotmail.com>
2015-09-16 22:00:03 +0200

    Update pts.sql

--- Benchmark/BM4/pts.sql
+++ Benchmark/BM4/pts.sql
@@ -1,5 +1,5 @@
 DROP TABLE if exists bpt;
 
 create table bpt as
-SELECT st_translate(randompointsinpolygon3d(st_3dextent(pipe3d.geometry3d)::geometry, 100), 0::double precision, 0::double precision, random() * (st_zmax(st_3dextent(pipe3d.geometry3d)) - st_zmin(st_3dextent(pipe3d.geometry3d))) + st_zmin(st_3dextent(pipe3d.geometry3d))) AS rpoint
+SELECT randompointsinpolygon3d(st_3dextent(pipe3d.geometry3d), 200) AS rpoint
 FROM pernis.pipe3d;
