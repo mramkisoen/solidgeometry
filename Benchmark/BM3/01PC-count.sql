21d7d1474eaef0cff4fa4439e2905823271702de
mramkisoen <myron_raj@hotmail.com>
2015-09-17 16:30:21 +0200

    Update 01PC-count.sql

--- Benchmark/BM3/01PC-count.sql
+++ Benchmark/BM3/01PC-count.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(a.*) total, COUNT(distinct a.pipeid) totalpipeid
 FROM geo3d.pipe3dp3 a JOIN benchpoints ON ST_3DDWithin(bbox3d, rpoint, 5)
-WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
+WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) IS TRUE;
