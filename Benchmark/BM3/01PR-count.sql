2c6ec6b71d3d09427bf69c7f63cce4faf6822cc7
mramkisoen <myron_raj@hotmail.com>
2015-09-17 16:31:00 +0200

    Update 01PR-count.sql

--- Benchmark/BM3/01PR-count.sql
+++ Benchmark/BM3/01PR-count.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(a.*) total, COUNT(distinct a.pipeid) totalpipeid
 FROM geo3d.pipe3drbb a JOIN benchpoints ON ST_3DDWithin(rbb, rpoint, 5)
-WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
+WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) IS TRUE;
