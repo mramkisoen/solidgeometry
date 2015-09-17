786cf04ad22fec09936466f9919989f201a5f540
mramkisoen <myron_raj@hotmail.com>
2015-09-17 16:30:48 +0200

    Update 01PC-time.sql

--- Benchmark/BM3/01PC-time.sql
+++ Benchmark/BM3/01PC-time.sql
@@ -1,4 +1,4 @@
 EXPLAIN ANALYZE
 SELECT a.*
 FROM geo3d.pipe3dp3 a JOIN benchpoints ON ST_3DDWithin(bbox3d, rpoint, 5)
-WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
+WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) IS TRUE;
