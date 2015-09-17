5b405773be707fe042d0f74a4ac1c10ab1746475
mramkisoen <myron_raj@hotmail.com>
2015-09-17 16:31:15 +0200

    Update 01PR-time.sql

--- Benchmark/BM3/01PR-time.sql
+++ Benchmark/BM3/01PR-time.sql
@@ -1,4 +1,4 @@
 EXPLAIN ANALYZE
 SELECT a.*
 FROM geo3d.pipe3drbb a JOIN benchpoints ON ST_3DDWithin(rbb, rpoint, 5)
-WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
+WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) IS TRUE;
