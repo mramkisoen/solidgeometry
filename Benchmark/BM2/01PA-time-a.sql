8fa50500abbe0738fcd43be5a1a5b1dd10ef64d5
mramkisoen <myron_raj@hotmail.com>
2015-10-14 11:02:48 +0200

    Delete 01PA-time-a.sql

--- Benchmark/BM2/01PA-time-a.sql
+++
@@ -1,4 +0,0 @@
-EXPLAIN ANALYZE
-SELECT a.*
-FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline(attributes), rpoint, 5)
-WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
