eacfc97fe437172de9fb43d1cdb9ac1f92e976f6
mramkisoen <myron_raj@hotmail.com>
2015-10-14 11:02:40 +0200

    Delete 01PA-count-a.sql

--- Benchmark/BM2/01PA-count-a.sql
+++
@@ -1,3 +0,0 @@
-SELECT COUNT(a.*) total, COUNT(distinct a.pipeid) totalpipeid
-FROM geo3d.pipe3dp a JOIN benchpoints ON ST_3DDWithin(ST_bboxline(attributes), rpoint, 5)
-WHERE ST_3DDWithin(ST_3Dreconstruct(attributes), rpoint, 5) is true;
